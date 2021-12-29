b4285ad678a1b755872b53f59001c6079273ae82
Michael Cann <mikepcannbass@gmail.com>
2021-12-29 12:11:05 -0700

    Most recent commit

--- src/env.js
+++ src/env.js
@@ -1,8 +1,7 @@
 /**
- * 
  * loads the .env file contents into the process.env var
  * When using this file, make sure that the .env is in the git ignore file.
  *  */ 
-import dotenv from "dotenv"; // imports the dotenv pkg
 
-dotenv.config(); 
+import dotenv from "dotenv"; // imports the dotenv pkg
+dotenv.config(); // configure the environment
